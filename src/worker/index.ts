7f6d507f560617f97688b4fda1331931aebe5eff
Felix Erdmann <hello@felixerdmann.com>
2023-09-06 16:33:32 +0200

    feat: worker try catch

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -12,28 +12,33 @@ export interface Payload {
 }
 
 export default async function (job: Job) {
-  const {
-    badgeClassEntityId,
-    createNotification,
-    email,
-    issuerEntityId,
-  }: Payload = job.data;
+  try {
+    const {
+      badgeClassEntityId,
+      createNotification,
+      email,
+      issuerEntityId,
+    }: Payload = job.data;
 
-  logger.info({ badgeClassEntityId, email, issuerEntityId, createNotification }, "Job payload")
+    logger.info({ badgeClassEntityId, email, issuerEntityId, createNotification }, "Job payload")
 
-  const client = new BadgrClient({
-    endpoint: config.BADGR_URL,
-    username: config.BADGR_USERNAME,
-    password: config.BADGR_PASSWORD,
-    admin: true,
-    client_id: config.BADGR_CLIENT_ID,
-    client_secret: config.BADGR_CLIENT_SECRET,
-  });
+    const client = new BadgrClient({
+      endpoint: config.BADGR_URL,
+      username: config.BADGR_USERNAME,
+      password: config.BADGR_PASSWORD,
+      admin: true,
+      client_id: config.BADGR_CLIENT_ID,
+      client_secret: config.BADGR_CLIENT_SECRET,
+    });
 
-  return await client.grant({
-    badgeClassEntityId,
-    email,
-    issuerEntityId,
-    createNotification,
-  });
+    return await client.grant({
+      badgeClassEntityId,
+      email,
+      issuerEntityId,
+      createNotification,
+    });
+  } catch (error) {
+    logger.error(error, "Error in job");
+    throw error;
+  }
 }
