711286e62bf7d33d21f6ae70aa18f2ae80b13f79
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-10-10 09:26:39 +0200

    Tweak elint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
-/* eslint-env node */
+/** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
+  root: true,
   extends: [
     "custom",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier"
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+  },
   plugins: ["@typescript-eslint"],
-  root: true,
 };
